14d84896b69bb887dea4d680c17d4b0697aa535a
tugascrown204 <gajdertevet@hotmail.com>
2025-08-15 14:43:24 +0330

    feat: initial commit for src/models/journalEntry.js

---
+++ src/models/journalEntry.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const JournalEntrySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('JournalEntry', JournalEntrySchema);
